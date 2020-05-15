e37185b92d5dc5d26b7f144a66df84319a1af1a8
cortezle <00029117@uca.edu.sv>
2020-05-14 18:34:46 -0600

    first commit

---
+++ src/main/java/com/uca/capasL5/CapasL5Application.java
@@ -0,0 +1,13 @@
+package com.uca.capasL5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapasL5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapasL5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/capasL5/CapasL5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capasL5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapasL5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
